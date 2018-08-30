ba50d9ade592f24d3fa8b5314bebd69f7393fa52
Luke Westby <lukewestby1@gmail.com>
2018-08-30 11:51:17 -0700

    upgrade example

--- examples/Main.elm
+++ examples/Main.elm
@@ -1,7 +1,6 @@
 module Main exposing (..)
 
-import BeautifulExample
-import Color
+import Browser
 import Html
 import Json.Decode as Decode
 import SweetPoll
@@ -14,7 +13,7 @@ type alias Model =
     }
 
 
-main : Program Never Model (SweetPoll.Msg String)
+main : Program () Model (SweetPoll.Msg String)
 main =
     let
         config =
@@ -26,38 +25,31 @@ main =
         sweetPollInit =
             SweetPoll.init config
     in
-    Html.program
+    Browser.element
         { init =
-            ( { data = Nothing
-              , error = Nothing
-              , sweetPoll = sweetPollInit |> Tuple.first
-              }
-            , sweetPollInit |> Tuple.second
-            )
+            \_ ->
+                ( { data = Nothing
+                  , error = Nothing
+                  , sweetPoll = sweetPollInit |> Tuple.first
+                  }
+                , sweetPollInit |> Tuple.second
+                )
         , update =
             \action model ->
                 case SweetPoll.update action model.sweetPoll of
                     { sweetPollModel, newData, error, cmd } ->
                         ( { sweetPoll = sweetPollModel
                           , data = newData
-                          , error = error |> Maybe.map toString
+                          , error = error |> Maybe.map Debug.toString
                           }
                         , cmd
                         )
         , view =
             \model ->
                 Html.div []
-                    [ Html.text ("new data: " ++ toString model.data)
+                    [ Html.text ("new data: " ++ Debug.toString model.data)
                     , Html.hr [] []
-                    , Html.text ("error: " ++ toString model.error)
+                    , Html.text ("error: " ++ Debug.toString model.error)
                     ]
-                    |> BeautifulExample.view
-                        { title = "elm-sweet-poll"
-                        , details = Just """HTTP polling with smart retry backoff."""
-                        , color = Just Color.red
-                        , maxWidth = 400
-                        , githubUrl = Just "https://github.com/NoRedInk/elm-sweet-poll"
-                        , documentationUrl = Just "http://package.elm-lang.org/packages/NoRedInk/elm-sweet-poll/latest/SweetPoll"
-                        }
         , subscriptions = \_ -> Sub.none
         }
